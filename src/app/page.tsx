4c7ccc4835a825dec6ebaa42042a3e9e0d9e84ee
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-09-06 23:52:46 +0530

    Hero page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { Button } from "@/components/ui/button";
+import { Vortex } from "@/components/ui/vortex";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <BackgroundGradient containerClassName="mx-10">
-        <Button size={"lg"} className="rounded-full">
-          <Link href="/home">Home</Link>
-        </Button>
-      </BackgroundGradient>
-    </div>
+    <Vortex>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div>
+          <p className="text-4xl font-bold bg-gradient-to-t from-white to-black bg-clip-text text-transparent">
+            NEW WAY TO MANAGE TASK
+          </p> 
+        </div>
+        <BackgroundGradient containerClassName="mx-10 mt-2">
+          <Button size={"lg"} className="rounded-full">
+            <Link href="/home">Create Task</Link>
+          </Button>
+        </BackgroundGradient>
+      </div>
+    </Vortex>
   );
 }
